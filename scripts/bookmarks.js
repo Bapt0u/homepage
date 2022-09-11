e96fbe6b3551bb75da4166368012ab1061f0e51b
Baptou <pporte.baptiste@protonmail.com>
2022-09-11 14:06:58 +0200

    Improvement :
    - Shadow under boxes are less opaque.
    - Erase useless bookmarks.

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -4,35 +4,23 @@ const bookmarks = [
     title: "Daily",
     links: [
       { name: "RSS",           url: "https://rss.b11e.net" },
-      { name: "GitHub",        url: "https://github.com" },
-      { name: "LinkedIn",      url: "https://linkedin.com" },
-      { name: "Hacker News",   url: "https://news.ycombinator.com/" },
+      { name: "Proton",        url: "https://account.proton.me/login" },
+      { name: "Google",  url: "https://accounts.google.com" }
     ],
   },
   {
     title: "Media",
     links: [
-      { name: "Youtube",      url: "https://youtube.com" },
+      { name: "Spotify",      url: "https://open.spotify.com/" },
       { name: "Netflix",      url: "https://netflix.com" },
-      { name: "Don't click",  url: "http://www.hat.net/abs/noclick/index.html" },
-      {name: "Spotify",       url: "https://open.spotify.com/" },
+      { name: "Hacker News",   url: "https://news.ycombinator.com/" }
     ],
   },
   {
     title: "HomeLab",
     links: [
       { name: "Traefik", url: "https://traefik.b11e.net" },
-      { name: "Wiki",    url: "https://wiki.b11e.net" },
-      { name: "Grafana", url: "https://grafana.b11e.net"},
       { name: "Photo",   url: "https://photo.b11e.net" }
-    ],
-  },
-  {
-    title: "Social",
-    links: [
-      { name: "Twitter",  url: "https://twitter.com" },
-      { name: "Reddit",   url: "https://reddit.com" },
-      { name: "wannadie", url: "https://playing.hypernom.com/" }
     ],
   },
 ];
